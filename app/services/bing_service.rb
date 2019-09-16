773bc56e9aad083d641f6ded3e7a09d4b90decae
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-09-15 18:43:33 -0600

    Create service for searching Bing Images given a location

---
+++ app/services/bing_service.rb
@@ -0,0 +1,18 @@
+class BingService
+  def get_images(location)
+    get_json("/images/search?q=#{location}")
+  end
+
+  private
+  def conn
+    Faraday.new(url: "https://api.cognitive.microsoft.com/bing/v7.0") do |f|
+      f.headers['Ocp-Apim-Subscription-Key'] = ENV['GOOGLE_API_KEY']
+      f.adapter Faraday.default_adapter
+    end
+  end
+
+  def get_json(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
