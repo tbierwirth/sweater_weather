60b3a037a73eee51941ab70fad6f73b6b6625b52
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-09-16 10:22:57 -0600

    Fix url of fixtures

--- spec/requests/api/v1/gif_request_spec.rb
+++ spec/requests/api/v1/gif_request_spec.rb
@@ -10,12 +10,16 @@ describe 'GIF API' do
     stub_request(:get, "https://maps.googleapis.com/maps/api/geocode/json?address=denver,co&key=#{ENV['GOOGLE_API_KEY']}").
       to_return(status: 200, body: geocoding)
 
-    mostly_cloudy = File.open('./spec/fixtures/partly_cloudy.json')
-    stub_request(:get, "api.giphy.com/v1/gifs/search?api_key=#{ENV['GIPHY_API_KEY']}?q=Mostly cloudy throughout the day.").
+    clear = File.open('./spec/fixtures/clear.json')
+    stub_request(:get, "https://api.giphy.com/v1/gifs/search?api_key=#{ENV['GIPHY_API_KEY']}&q=Clear%20throughout%20the%20day.").
+      to_return(status: 200, body: clear)
+
+    mostly_cloudy = File.open('./spec/fixtures/mostly_cloudy.json')
+    stub_request(:get, "https://api.giphy.com/v1/gifs/search?api_key=#{ENV['GIPHY_API_KEY']}&q=Mostly%20cloudy%20throughout%20the%20day.").
       to_return(status: 200, body: mostly_cloudy)
 
     partly_cloudy = File.open('./spec/fixtures/partly_cloudy.json')
-    stub_request(:get, "api.giphy.com/v1/gifs/search?api_key=#{ENV['GIPHY_API_KEY']}?q=Partly cloudy throughout the day.").
+    stub_request(:get, "https://api.giphy.com/v1/gifs/search?api_key=#{ENV['GIPHY_API_KEY']}&q=Partly%20cloudy%20throughout%20the%20day.").
       to_return(status: 200, body: partly_cloudy)
   end
   it 'sends forecast given city,state' do
@@ -24,6 +28,7 @@ describe 'GIF API' do
     expect(response).to be_successful
 
     parsed = JSON.parse(response.body, symbolize_names: true)
+    binding.pry
     expect(parsed[:data][:images][0][:time]).to eq("1568527200")
     expect(parsed[:data][:images][0][:summary]).to eq("Mostly cloudy throughout the day.")
     expect(parsed[:data][:images][0][:url]).to eq("https://giphy.com/gifs/dark-castle-63xBFHKNVjZlu")
