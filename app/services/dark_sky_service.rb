97a90237d503a8cb2ff89c67d64caed6548aabd7
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-09-17 16:15:35 -0600

    Add a forecast_at method to darksky service to return forecast of a location at a given time

--- app/services/dark_sky_service.rb
+++ app/services/dark_sky_service.rb
@@ -3,6 +3,10 @@ class DarkSkyService
     get_json("/forecast/#{ENV['DARKSKY_API_KEY']}/#{lat},#{lng}")
   end
 
+  def forecast_at(time, lat, lng)
+    get_json("/forecast/#{ENV['DARKSKY_API_KEY']}/#{lat},#{lng},#{time}")
+  end
+
   private
   def conn
     Faraday.new(url: "https://api.darksky.net") do |f|
